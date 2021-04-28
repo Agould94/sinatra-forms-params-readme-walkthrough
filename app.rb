4baacd303d92d44004d700b641a44aaa06e1d62b
Alexander Gould <alexander.gould94@gmail.com>
2021-04-28 12:14:28 -0700

    commit tree b7496476e435a6a5f2b84ca1799172174bc2b4e2

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  set :views, settings.root + '/4:26-4:30/sinatra-forms-params-readme-walkthrough/views'
 
   get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
-
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 end
